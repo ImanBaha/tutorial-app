234cd82fa7d509c0390cc016b2f9c4b06fef5f25
Iman Baha <mhdnurimanbaha@gmail.com>
2023-12-11 16:38:41 +0800

    episod8

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<! doctype html>
+
+<title> My Blog </title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+    <h1> <a href="/post"> My First posts </a></h1>
+
+    <p>
+    LoremDo adipisicing occaecat ut culpa. Et in et aute anim proident sit et excepteur ex labore. Officia enim cillum magna velit id aute nulla aliquip dolor. Eu laborum sint adipisicing adipisicing do tempor. ipsum dolor sit amet consectetur, adipisicing elit. At, illum provident commodi autem maiores ratione in ad eum saepe quisquam. Sint impedit totam aspernatur dignissimos ratione libero illo debitis omnis?
+    </p> 
+    
+    </article>
+
+    <article>
+        <h1><a href="/post"> My Second posts </a></h1>
+    
+        <p>
+        LoremDo adipisicing occaecat ut culpa. Et in et aute anim proident sit et excepteur ex labore. Officia enim cillum magna velit id aute nulla aliquip dolor. Eu laborum sint adipisicing adipisicing do tempor. ipsum dolor sit amet consectetur, adipisicing elit. At, illum provident commodi autem maiores ratione in ad eum saepe quisquam. Sint impedit totam aspernatur dignissimos ratione libero illo debitis omnis?
+        </p> 
+        
+        </article>
+
+        <article>
+            <h1><a href="/post"> My Third posts </a></h1>
+        
+            <p>
+            LoremDo adipisicing occaecat ut culpa. Et in et aute anim proident sit et excepteur ex labore. Officia enim cillum magna velit id aute nulla aliquip dolor. Eu laborum sint adipisicing adipisicing do tempor. ipsum dolor sit amet consectetur, adipisicing elit. At, illum provident commodi autem maiores ratione in ad eum saepe quisquam. Sint impedit totam aspernatur dignissimos ratione libero illo debitis omnis?
+            </p> 
+            
+            </article>
+
+</body>
+
